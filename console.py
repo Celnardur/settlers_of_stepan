7fc369806cd5d63328da89d486c64f8dfb6cbbb2
Celnardur <celnardur@protonmail.com>
2021-04-04 17:01:38 -0400

    Added s command to console and s ap to add players

--- console.py
+++ console.py
@@ -6,6 +6,22 @@ import pprint
 import sys
 import json
 
+def scmd(args):
+    script = []
+    if args[0] == 'ap':
+        script.append('new_game')
+        script.append('add_player name "Aaron" color [255,0,0] order 0')
+        script.append('add_player name "James" color [0,0,255] order 1')
+        script.append('add_player name "Kyle" color [255,0,255] order 2')
+        script.append('add_player name "Jack" color [0,255,0] order 3')
+        script.append('print players')
+
+    ret = ''
+    for command in script:
+        ret = process_command(command)
+
+    return ret
+
 def process_command(command):
     if command == "":
         return get_state_string(api.state)
@@ -22,24 +38,23 @@ def process_command(command):
         args_dict[arg] = value
         arg_n += 2
 
-    try:
-        if command == '/print':
-            if len(args) < 2:
-                return "Needs item to print"
-            if not args[1] in api.state:
-                return 'Cannot print ' + args[1]
+    if command == '/s':
+        return scmd(args[1:])
+    elif command == '/print':
+        if len(args) < 2:
+            return get_state_string(api.state)
+        if not args[1] in api.state:
+            return 'Cannot print ' + args[1]
 
-            pp = pprint.PrettyPrinter(indent=4)
-            return pp.pformat(api.state[args[1]])
+        pp = pprint.PrettyPrinter(indent=4)
+        return pp.pformat(api.state[args[1]])
+    else:
+        (code, message) = api.process(command, args_dict)
+        if code == 200:
+            api.get_notifications(command, args_dict)
+            return str(code) + ': ' + str(message) + '\n' + get_state_string(api.state)
         else:
-            (code, message) = api.process(command, args_dict)
-            if code == 200:
-                api.get_notifications(command, args_dict)
-                return str(code) + ': ' + str(message) + '\n' + get_state_string(api.state)
-            else:
-                return str(code) + ': ' + str(message) + '\n'
-    except:
-        return "API Error"
+            return str(code) + ': ' + str(message) + '\n'
 
 
 if __name__ == '__main__':
